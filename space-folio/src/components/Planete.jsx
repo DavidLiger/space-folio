ad3ed88cf7320c83c49a609333db5280df134890
David LIGER <liger@biotrade.fr>
2025-02-25 16:29:57 +0100

    feature: add rot to planets

--- space-folio/src/components/Planete.jsx
+++ space-folio/src/components/Planete.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useSpring, animated } from "@react-spring/three";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 export default function Planete({ position, nom, onClick }) {
   const [hover, setHover] = useState(false);
+  const planetRef = useRef(); // Référence pour contrôler la rotation
 
   // Animation d’agrandissement au survol
   const { scale } = useSpring({
@@ -17,16 +19,24 @@ export default function Planete({ position, nom, onClick }) {
   // Gestion du curseur
   const handlePointerOver = () => {
     setHover(true);
-    document.body.style.cursor = "pointer"; // Change le curseur
+    document.body.style.cursor = "pointer";
   };
 
   const handlePointerOut = () => {
     setHover(false);
-    document.body.style.cursor = "default"; // Rétablit le curseur
+    document.body.style.cursor = "default";
   };
+
+  // Animation de rotation continue
+  useFrame(() => {
+    if (planetRef.current) {
+      planetRef.current.rotation.y += 0.002; // Ajuste la vitesse de rotation
+    }
+  });
 
   return (
     <animated.mesh
+      ref={planetRef}
       scale={scale}
       position={position}
       onClick={onClick}
