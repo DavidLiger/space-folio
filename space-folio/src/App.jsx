ff1e0ed988ee45e3327420f4d6c492f1f4b80fdb
David LIGER <liger@biotrade.fr>
2025-02-25 16:08:17 +0100

    feature: replace sphere by .glb for planets

--- space-folio/src/App.jsx
+++ space-folio/src/App.jsx
@@ -22,8 +22,8 @@ export default function Scene() {
         
         {/* 💡 Lumières */}
         <ambientLight intensity={0.5} />
-        <Planete position={[2, 0, 0]} couleur="blue" nom="React" onClick={() => handleClick("React", "Framework JS moderne.")} />
-        <Planete position={[-2, 0, 0]} couleur="red" nom="PHP" onClick={() => handleClick("PHP", "Backend robuste et éprouvé.")} />
+        <Planete position={[2, 0, 0]} nom="React" onClick={() => handleClick("React", "Framework JS moderne.")} />
+        <Planete position={[-3, 0, 0]} nom="PHP" onClick={() => handleClick("PHP", "Backend robuste et éprouvé.")} />
         <directionalLight position={[5, 10, 5]} intensity={1} />
 
         {/* 🚀 Le vaisseau */}
--- space-folio/src/components/Planete.jsx
+++ space-folio/src/components/Planete.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { useSpring, animated } from "@react-spring/three";
-import { Sphere } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
-export default function Planete({ position, couleur, nom, onClick }) {
+export default function Planete({ position, nom, onClick }) {
   const [hover, setHover] = useState(false);
 
   // Animation d’agrandissement au survol
   const { scale } = useSpring({
     scale: hover ? 1.2 : 1,
   });
+
+  // Chargement du modèle en fonction du nom de la planète
+  const modelPath = nom === "PHP" ? "/models/cute_little_planet.glb" : "/models/low_poly_planet.glb";
+  const { scene } = useGLTF(modelPath);
 
   return (
     <animated.mesh
@@ -18,9 +22,7 @@ export default function Planete({ position, couleur, nom, onClick }) {
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
-      <Sphere args={[1, 32, 32]}>
-        <meshStandardMaterial color={couleur} />
-      </Sphere>
+      <primitive object={scene} />
     </animated.mesh>
   );
 }
