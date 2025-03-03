01c4838f07b5f4326d4c6ad1390d1ac6ae2ddb54
David LIGER <liger@biotrade.fr>
2025-03-03 15:45:24 +0100

    feature: planet modale slide from the right

--- space-folio/src/components/Modale.jsx
+++ space-folio/src/components/Modale.jsx
@@ -1,12 +1,23 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import "./Modale.css"; // Ajoute du style
+import { useEffect, useState } from "react";
 
 export default function Modale({ open, onClose, title, content }) {
+  const [animationClass, setAnimationClass] = useState("");
+
+  useEffect(() => {
+    if (open) {
+      setAnimationClass("slide-in");
+    } else {
+      setAnimationClass("slide-out");
+    }
+  }, [open]);
+
   return (
     <Dialog.Root open={open} onOpenChange={onClose}>
       <Dialog.Portal>
         {/* <Dialog.Overlay className="modal-overlay" /> */}
-        <Dialog.Content className="modal-content slide-in">
+        <Dialog.Content className={`modal-content ${animationClass}`}>
           <Dialog.Title>{title}</Dialog.Title>
           <Dialog.Description>{content}</Dialog.Description>
           <Dialog.Close className="modal-close">Fermer</Dialog.Close>
